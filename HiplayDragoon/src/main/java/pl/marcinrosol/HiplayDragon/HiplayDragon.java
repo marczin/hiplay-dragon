5b593b527d1c29f7a0ac97071cff21458d2b5baa
Marczin <marcinrosol1997@gmail.com>
2020-11-24 11:57:03 +0100

    Scoreboard

--- HiplayDragoon/src/main/java/pl/marcinrosol/HiplayDragon/HiplayDragon.java
+++ HiplayDragoon/src/main/java/pl/marcinrosol/HiplayDragon/HiplayDragon.java
@@ -1,8 +1,14 @@
 package pl.marcinrosol.HiplayDragon;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
 import pl.marcinrosol.HiplayDragon.commands.*;
 import pl.marcinrosol.HiplayDragon.entities.CrystalCfg;
 import pl.marcinrosol.HiplayDragon.entities.DestroyedCrystal;
@@ -10,6 +16,7 @@ import pl.marcinrosol.HiplayDragon.entities.GameCfg;
 import pl.marcinrosol.HiplayDragon.listeners.*;
 import pl.marcinrosol.HiplayDragon.tasks.DragonModeTask;
 import pl.marcinrosol.HiplayDragon.tasks.SpawnCrystalTask;
+import pl.marcinrosol.HiplayDragon.tasks.UpdateScoreboardTask;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +28,9 @@ public class HiplayDragon extends JavaPlugin {
     public FileConfiguration config = getConfig();
     public GameCfg gameCfg;
     public List<DestroyedCrystal> destroyedCrystalList = new CopyOnWriteArrayList<>();
+    public ScoreboardManager scoreboardManager = Bukkit.getScoreboardManager();
+    public Scoreboard scoreboard = scoreboardManager.getMainScoreboard();
+    public Objective objective = scoreboard.registerNewObjective("DragonBoard", "");
 
     @Override
     public void onEnable() {
@@ -31,6 +41,7 @@ public class HiplayDragon extends JavaPlugin {
         CrystalCfg.setup();
         CrystalCfg.get().options().copyDefaults(true);
         SpawnCrystalTask.crystalTask();
+        prepareScoreboard();
         System.out.println("HiPlay Dragon enabled");
     }
 
@@ -54,6 +65,11 @@ public class HiplayDragon extends JavaPlugin {
         getCommand("killdragon").setExecutor(new KillDragonCommand());
         getCommand("status").setExecutor(new GameStatusCommand());
         getCommand("spawn").setExecutor(new SpawnCommand());
+    }
+
+    private void prepareScoreboard(){
+        Score mobKilled = objective.getScore(ChatColor.RED + "Mob killed: "+(gameCfg.getMobProgress()-1));
+        mobKilled.setScore(1);
     }
 
 }
---
+++ HiplayDragoon/src/main/java/pl/marcinrosol/HiplayDragon/tasks/UpdateScoreboardTask.java
@@ -0,0 +1,16 @@
+package pl.marcinrosol.HiplayDragon.tasks;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import pl.marcinrosol.HiplayDragon.HiplayDragon;
+
+public final class UpdateScoreboardTask {
+
+    public static void UpdateScoreboardTask(){
+
+
+    }
+
+}
+
